9b1d1ae78c2aa1a50abdaa06548a8b7767a2b3c0
Matias Nuñez <Matiasne45@gmail.com>
2023-03-10 18:45:57 -0300

    Add salesforce library

--- salesforce/auth.py
+++ salesforce/auth.py
@@ -1,5 +1,7 @@
 # lead-crm-library/salesforce/auth.py
+from simple_salesforce import Salesforce
 
 def authenticate(username, password, security_token):
     # Código para autenticarse con Salesforce usando credenciales de usuario y un token de seguridad
-    pass
+    f = Salesforce(username=username, password=password, security_token=security_token)
+    return f
--- salesforce/leads.py
+++ salesforce/leads.py
@@ -6,14 +6,13 @@ from .utils import normalize_lead_data
 
 def add_lead(name, email, phone, car_model, suspension_type, payment_method, contact_preferred_time):
     try:
-        access_token = authenticate("username", "password", "security_token")
+        sfClient = authenticate("username", "password", "security_token")
 
         # Normalizar los datos del lead antes de agregarlos a Salesforce
         normalized_data = normalize_lead_data(name, email, phone, car_model, suspension_type, payment_method, contact_preferred_time)
 
         # Código para agregar un lead a Salesforce usando el token de acceso y los datos normalizados
-        pass
+        sfClient.Lead.create(normalized_data)
 
     except Exception as e:
         raise SalesforceError(f"Error al agregar lead a Salesforce: {str(e)}")
-
